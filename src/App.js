b54b43299812f144b926e98bc0ff447dff9f0ee7
Anna Kijania <kijania.anna@gmail.com>
2021-02-27 10:35:10 +0100

    Add registration view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import './styles/global.scss';
 import  MainLayout  from './components/layout/MainLayout/MainLayout';
 import  UserPage  from './components/views/UserPage/UserPage';
 import  Login  from './components/views/Login/Login';
+import  Registration  from './components/views/Registration/Registration';
 import  NotFound  from './components/views/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
       <BrowserRouter>
         <MainLayout>
           <Switch>
             <Route exact path='/' component={Login} />
+            <Route exact path='/register' component={Registration} />
             <Route exact path='/:id' component={UserPage} />
-            {/* <Route exact path='/register' component={Register} /> */}
             <Route path='*' component={NotFound} />
           </Switch>
         </MainLayout>
       </BrowserRouter>
-
     </div>
   );
 }
---
+++ src/components/views/Registration/Registration.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './Registration.module.scss';
+import clsx from 'clsx';
+
+const Registration = (className) => {
+  return (
+    <div className={clsx(className, styles.root)}>
+      <h2>Please register</h2>
+      <form method="POST">
+        <label>
+          <p>First name</p>
+          <input type="text" name="first-name" />
+        </label>
+        <label>
+          <p>Family name</p>
+          <input type="text" name="family-name"/>
+        </label>
+        <label>
+          <p>E-mail</p>
+          <input type="email" name="email" required />
+        </label>
+        <label>
+          <p>Username</p>
+          <input type="text" name="username" required />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" name="password" required />
+        </label>
+        <div>
+          <button className={styles.btn} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
