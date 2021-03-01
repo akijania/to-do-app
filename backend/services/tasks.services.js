5867aabc2021e012bdfdda43cceb9902396c9b71
Anna Kijania <kijania.anna@gmail.com>
2021-03-01 22:10:14 +0100

    Remove user from db and edit task

--- backend/services/tasks.services.js
+++ backend/services/tasks.services.js
@@ -26,7 +26,7 @@ async function create(tasks) {
     [tasks.task, tasks.userId]
   );
 
-  let message = 'Error in creating user';
+  let message = 'Error in creating task';
 
   if (result.affectedRows) {
     message = 'New task created successfully';
@@ -35,7 +35,43 @@ async function create(tasks) {
   return { message };
 }
 
+async function update(id, tasks){
+  const result = await db.query(
+    `UPDATE tasks 
+    SET task=?
+    WHERE id=?`, 
+    [
+      tasks.task, id,
+    ]
+  );
+
+  let message = 'Error in updating task';
+
+  if (result.affectedRows) {
+    message = 'Task updated successfully';
+  }
+
+  return {message};
+}
+
+async function remove(id){
+  const result = await db.query(
+    `DELETE FROM tasks WHERE id=?`, 
+    [id]
+  );
+
+  let message = 'Error in deleting task';
+
+  if (result.affectedRows) {
+    message = 'Task deleted successfully';
+  }
+
+  return {message};
+}
+
 module.exports = {
   getTasks,
   create,
+  update,
+  remove,
 };
